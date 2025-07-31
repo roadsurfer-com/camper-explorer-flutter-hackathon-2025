64eab004b03d5563eadc48f749ebc9fb413bd546
Vitalie Frunze <vitalie.frunze@roadsurfer.com>
2025-07-31 16:02:29 +0200

    Save work

--- lib/feature_details_page.dart
+++ lib/feature_details_page.dart
@@ -2,18 +2,40 @@ import 'package:flutter/material.dart';
 
 class FeatureDetailPage extends StatelessWidget {
   final String feature;
+  final String imageAsset;
+  final String subtitle;
 
-  const FeatureDetailPage({super.key, required this.feature});
+  const FeatureDetailPage({
+    super.key,
+    required this.feature,
+    required this.imageAsset,
+    required this.subtitle,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(feature)),
-      body: Center(
-        child: Text(
-          'More info about $feature',
-          style: const TextStyle(fontSize: 24),
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 2.0),
+            child: Image.asset(
+              imageAsset,
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Text(
+              subtitle,
+              style: const TextStyle(fontSize: 24),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
       ),
     );
   }
